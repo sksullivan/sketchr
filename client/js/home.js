33e3fd2d91fdb4696c9dec87aad26b8252f89a2b
Stephen Sulivan <sullstecclub@gmail.com>
2014-02-08 11:05:35 -0500

    heroism

--- client/js/home.js
+++ client/js/home.js
@@ -29,11 +29,12 @@ DrawItem.prototype.draw = function () {
 }
 
 DrawItem.prototype.contains = function (event) {
-	if (event.targetTouches) {
+	/*if (event.targetTouches) {
 		event.x = event.targetTouches[0].pageX;
 		event.y = event.targetTouches[0].pageY;
-	}
-	return event.y < this.y+this.height && event.y > this.y && event.x < this.x+this.width && event.x > this.x;
+	}*/
+	//return event.y < this.y+this.height && event.y > this.y && event.x < this.x+this.width && event.x > this.x;
+	return event.targetTouches[0].pageY < this.y+this.height && event.targetTouches[0].pageY > this.y && event.targetTouches[0].pageX < this.x+this.width && event.targetTouches[0].pageX > this.x;
 }
 
 DrawItem.prototype.onMouseDown = function () {
@@ -79,11 +80,11 @@ Road.prototype.onMouseDown = function () {
 }
 
 Road.prototype.contains = function (event) {
-	if (event.targetTouches) {
+	/*if (event.targetTouches) {
 		event.x = event.targetTouches[0].pageX;
 		event.y = event.targetTouches[0].pageY;
-	}
-	return event.y < this.y+this.height/2 && event.y > this.y-this.height/2 && event.x < this.x+this.width/2 && event.x > this.x-this.width/2;
+	}*/
+	return event.targetTouches[0].pageY < this.y+this.height/2 && event.targetTouches[0].pageY > this.y-this.height/2 && event.targetTouches[0].pageX < this.x+this.width/2 && event.targetTouches[0].pageX > this.x-this.width/2;
 }
 
 Road.prototype.draw = function () {
