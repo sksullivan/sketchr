f68c1313a91d58799f495fc49d6073e74cff3ea4
Stephen Sulivan <sullstecclub@gmail.com>
2014-02-08 04:45:25 -0500

    what

--- client/js/index.js
+++ client/js/index.js
@@ -94,6 +94,7 @@ function touchDown(event) {
 	road.pos1 = Pos1;
 	road.pos2 = Pos2;
 	items.push(road);
+	$('#info').text(road.pos1.x+", "+road.pos1.y+" - "+road.pos2.x+", "+road.pos2.y);
 	Pos1.x = 0;
 	Pos1.y = 0;
 	Pos2.x = 0;
