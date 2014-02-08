e979d1c06861ab9ed47a57c57252677d68eb23e1
Stephen Sulivan <sullstecclub@gmail.com>
2014-02-08 04:05:22 -0500

    Added touch support.

--- client/js/index.js
+++ client/js/index.js
@@ -64,17 +64,17 @@ function touchDown(event) {
 		slope = (roadPos2.y-roadPos1.y)/(roadPos2.x-roadPos1.x);
 		$('#info').text(Math.atan(slope));
 		if (Math.atan(slope)<.52 && Math.atan(slope)>-.25) {
-			$('#info').text(Math.atan(slope)+" Should be flat line");
 			roadPos1.y = (roadPos2.y+roadPos1.y)/2;
 			roadPos2.y = (roadPos2.y+roadPos1.y)/2;
+			$('#info').text(roadPos1.x+", "roadPos1.y+" - "+roadPos2.x+", "roadPos2.y);
 		} else if (Math.atan(slope)<1.05 && Math.atan(slope)>-1.05) {
-			$('#info').text(Math.atan(slope)+" Should be diagonal line");
 			ydiff = roadPos2.y-roadPos1.y;
 			roadPos2.x = roadPos1.x+ydiff;
+			$('#info').text(roadPos1.x+", "roadPos1.y+" - "+roadPos2.x+", "roadPos2.y);
 		} else {
-			$('#info').text(Math.atan(slope)+" Should be vertical line");
 			roadPos1.x = (roadPos2.x+roadPos1.x)/2;
 			roadPos2.x = (roadPos2.x+roadPos1.x)/2;
+			$('#info').text(roadPos1.x+", "roadPos1.y+" - "+roadPos2.x+", "roadPos2.y);
 		}
 		road.pos1 = roadPos1;
 		road.pos2 = roadPos2;
