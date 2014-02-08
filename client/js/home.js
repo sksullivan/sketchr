23ca0ede3a7416306f25768045cd92cf562c4e0f
Stephen Sulivan <sullstecclub@gmail.com>
2014-02-08 14:19:02 -0500

    None left :(

--- client/js/home.js
+++ client/js/home.js
@@ -72,8 +72,6 @@ function Road (x,y,x2,y2,size,name,ctx) {
 	this.name = name;
 }
 
-Road.prototype = new DrawItem();
-
 Road.prototype.onMouseDown = function () {
 	$('#info').text("pressed2");
 }
@@ -171,6 +169,34 @@ UCarGenrator.prototype.onMouseDown = function () {
 	}
 }
 
+// Road Generator Class
+
+function RoadGenrator (x,y,heading,size,imageName,name,ctx,type) {
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	this.heading = heading; // In degrees
+	if (imageName != null) {
+		this.image = new Image();
+		this.image.src = imageName;
+		this.image.owner = this;
+		this.image.onload = function () {
+			this.owner.draw();
+			this.owner.width = this.width;
+			this.owner.height = this.height;
+		}
+	}
+	this.name = name;
+	this.ctx = ctx;
+	this.type = type;
+}
+
+RoadGenrator.prototype = new DrawItem();
+
+RoadGenrator.prototype.onMouseDown = function () {
+	placeMode = "road";
+}
+
 // Setup and Main code
 
 drawItemList = [];
@@ -180,21 +206,22 @@ cvs = null;
 placeMode = null;
 carGenerator = null;
 uCarGenerator = null;
+roadGenerator = null;
+
+tempRoad = null;
 
 $(document).ready(function () {
 	// Initialize scene and Draw Items
 	cvs = document.getElementById('canvas');
 	cvs.width  = $(window).width();
-	cvs.height  = $(window).height()-200;
+	cvs.height  = $(window).height();
 	ctx = document.getElementById('canvas').getContext('2d');
-	car = new DrawItem(0,150,0,100,"/assets/redcar.png","the car",ctx);
-	road = new Road(500,150,500,500,130,"the road",ctx);
 	carGenerator = new CarGenerator(0,0,0,100,"/assets/redcar.png","the car generator",ctx);
 	uCarGenerator = new UCarGenrator(220,0,0,100,"/assets/graycar.png","the uninvolved car generator",ctx);
-	drawItemList.push(car);
-	drawItemList.push(road);
+	roadGenerator = new RoadGenrator(440,0,0,100,"/assets/road.png","the road generator",ctx);
 	drawItemList.push(carGenerator);
 	drawItemList.push(uCarGenerator);
+	drawItemList.push(roadGenerator);
 
 	canvas.addEventListener("mousedown", mouseDown, false);
 	canvas.addEventListener("mouseup", mouseUp, false);
@@ -207,7 +234,6 @@ function mouseDown (event) {
 	event.preventDefault();
 	for (i=0;i<drawItemList.length;i++) {
 		if (drawItemList[i].contains(event) && drawItemList[i].toString() != 'Road') {
-			console.log(typeof drawItemList[i]);
 			drawItemList[i].onMouseDown();
 			return;
 		}
@@ -221,9 +247,44 @@ function mouseDown (event) {
 			uCar = new DrawItem(event.x-uCarGenerator.width/2,event.y-uCarGenerator.height/2,uCarGenerator.heading,100,"/assets/graycar.png","the car",this.ctx);
 			drawItemList.push(uCar);
 			break;
+		case "road":
+			tempRoad = new Road(event.x,event.y,0,0,130,"/assets/road.png","the road",this.ctx);
+			placeMode = "road2";
+			break;
+		case "road2":
+			x2 = event.x;
+			y2 = event.y;
+			x1 = tempRoad.x;
+			y1 = tempRoad.y;
+			slope = (y2-y1)/(x2-x1);
+			angle = -Math.atan(slope)/Math.PI*180;
+			if (angle > 55) {
+				x1 = (x2+x1)/2;
+				x2 = x1;
+			} else if (angle > 35) {
+				ydiff = y2-y1;
+				x2 = x1-ydiff;
+			} else if (angle > -35) {
+				y1 = (y2+y1)/2;
+				y2 = y1;
+			} else if (angle > -55) {
+				ydiff = y2-y1;
+				x2 = x1+ydiff;
+			} else { 
+				x1 = (x2+x1)/2;
+				x2 = x1;
+			}
+			tempRoad.x = x1;
+			tempRoad.y = y1;
+			tempRoad.x2 = x2;
+			tempRoad.y2 = y2;
+			drawItemList.push(tempRoad);
+			placeMode = "road";
+			break;
 		default:
 			break;
 	}
+	redraw();
 }
 
 function mouseUp (event) {
@@ -238,6 +299,11 @@ function mouseUp (event) {
 
 function redraw () {
 	ctx.clearRect(0,0,$(window).width(),$(window).height());
+	ctx.moveTo(0, 150);
+	ctx.lineTo($(window).width(), 150);
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = '#000000';
+	ctx.stroke();
 	drawItemList.forEach(function (drawItem) {
 		drawItem.draw();
 	});
@@ -246,10 +312,4 @@ function redraw () {
 		car.rotate(45);
 		redraw();
 	}, 1000);*/
-}
-
-// Helper Methods
-
-function isInTouch (event,element) {
-	return event.targetTouches[0].pageX < element.x+element.width && event.targetTouches[0].pageX > element.x && event.targetTouches[0].pageY < element.y+element.height && event.targetTouches[0].pageY > element.y;
 }
