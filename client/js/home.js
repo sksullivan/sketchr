64aa5a5c59c0a85b1418a54bb2e0624871f68813
Stephen Sulivan <sullstecclub@gmail.com>
2014-02-08 19:51:22 -0500

    I am god. New roads ALWAYS GO BEHIND CARS. DID I MENTION I AM GOD. Also fixed rotations transformations issue.

--- client/js/home.js
+++ client/js/home.js
@@ -362,7 +362,7 @@ $(document).ready(function () {
 	canvas.addEventListener("mousedown", mouseDown, false);
 	canvas.addEventListener("touchstart", mouseDragDown, false);
 	canvas.addEventListener("touchend", mouseEnd, false);
-	canvas.addEventListener("touchcancel", mouseCancel, false);
+	//canvas.addEventListener("touchcancel", mouseCancel, false);
 	canvas.addEventListener("touchmove", mouseDrag, false);
 
 	redraw();
@@ -379,12 +379,13 @@ function mouseDragDown (event) {
 }
 
 function mouseEnd (event) {
+	event.preventDefault();
 	$('#info').text(event.targetTouches[0].pageX+" END "+event.targetTouches[0].pageY);
 }
 
-function mouseCancel (event) {
+/*function mouseCancel (event) {
 	$('#info').text(event.targetTouches[0].pageX+" CANCEL "+event.targetTouches[0].pageY);
-}
+}*/
 
 function mouseDown (event) {
 	event.preventDefault();
