3c122e4b48a40ae57036ee8bdf2a1a1694ba4d49
Stephen Sulivan <sullstecclub@gmail.com>
2014-02-08 19:39:47 -0500

    I am god. New roads ALWAYS GO BEHIND CARS. DID I MENTION I AM GOD. Also fixed rotations transformations issue.

--- client/js/home.js
+++ client/js/home.js
@@ -368,12 +368,12 @@ $(document).ready(function () {
 
 function mouseDrag (event) {
 	event.preventDefault();
-	$('#info').text(event.targetTouches[0].x+" MOVE "+event.targetTouches[0].y);
+	$('#info').text(event.targetTouches[0].pageX+" MOVE "+event.targetTouches[0].pageY);
 }
 
 function mouseDragDown (event) {
 	event.preventDefault();
-	$('#info').text(event.targetTouches[0].x+" START "+event.targetTouches[0].y);
+	$('#info').text(event.targetTouches[0].pageX+" START "+event.targetTouches[0].pageY);
 }
 
 function mouseDown (event) {
